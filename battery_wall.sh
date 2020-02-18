27ee6afdd3034d23fc463a5132935f2a61ee9d8c
adi1090x <adi1090x@gmail.com>
2020-02-18 21:00:27 +0530

    Files Added

---
+++ battery_wall.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Author : Aditya Shakya (adi1090x)
+
+DIR="/usr/share/battery-wallpaper"
+
+BAT="$(ls /sys/class/power_supply/ | grep -i BAT | head -n 1)"
+AC="$(ls /sys/class/power_supply/ | grep -i AC | head -n 1)"
+
+BATTERY="$(cat /sys/class/power_supply/$BAT/capacity)"
+CHARGE="$(cat /sys/class/power_supply/$AC/online)"
+
+function battery {
+	if [[ $CHARGE == 1 ]]; then
+       hsetroot -fill $DIR/images/charge_1.png ; sleep 0.8
+       hsetroot -fill $DIR/images/charge_2.png ; sleep 0.8
+       hsetroot -fill $DIR/images/charge_3.png ; sleep 0.8
+       hsetroot -fill $DIR/images/charge_4.png ; sleep 0.8
+       hsetroot -fill $DIR/images/charge_5.png ; sleep 0.8
+          
+	elif [[ $BATTERY -ge 5 ]] && [[ $BATTERY -le 20 ]]; then
+       hsetroot -fill $DIR/images/battery_1.png ; sleep 5
+	elif [[ $BATTERY -ge 20 ]] && [[ $BATTERY -le 40 ]]; then
+       hsetroot -fill $DIR/images/battery_2.png ; sleep 5
+	elif [[ $BATTERY -ge 40 ]] && [[ $BATTERY -le 60 ]]; then
+       hsetroot -fill $DIR/images/battery_3.png ; sleep 5
+	elif [[ $BATTERY -ge 60 ]] && [[ $BATTERY -le 80 ]]; then
+       hsetroot -fill $DIR/images/battery_4.png ; sleep 5
+	elif [[ $BATTERY -ge 80 ]] && [[ $BATTERY -le 100 ]]; then
+       hsetroot -fill $DIR/images/battery_5.png ; sleep 5
+    fi
+}
+
+while true;do
+	battery && exec $DIR/battery_wall.sh
+done
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+## Author : Aditya Shakya (adi1090x)
+
+# Colors
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[0;36m'
+G='\033[1;32m'
+W='\033[1;37m'
+Y='\033[1;33m'
+
+# Path
+DIR="$(pwd)"
+DES="/usr/share"
+
+echo
+echo -e $Y" [*] Installing... "$C
+echo
+sudo mkdir -p $DES/battery-wallpaper && sudo cp -r $DIR/images $DES/battery-wallpaper && sudo cp -r $DIR/battery_wall.sh $DES/battery-wallpaper
+sudo chmod +x $DES/battery-wallpaper/battery_wall.sh
+sudo ln -s $DES/battery-wallpaper/battery_wall.sh /usr/bin/bwall
+cd .. && rm -rf battery-wallpaper
+echo
+echo -e $Y" [*] Installed. Execute 'bwall' to Run."$C
+echo
