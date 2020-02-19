df974667a991e051c68bd95ccfdb750856fb78ee
oli <oli.shubham@gmail.com>
2020-02-19 17:44:13 +0530

    Refactored core code
    File: battery_wall.sh
    
    Changes:
    - Made code more modular using functions
    - Migrated if-elif equality based logic to division based
    - removed exec call, instead called main function infintely
    
    Important:
    - Code is not tested as I don't have a machine right now (only cloud native VMs)

--- battery_wall.sh
+++ battery_wall.sh
@@ -7,30 +7,38 @@ DIR="/usr/share/battery-wallpaper"
 BAT="$(ls /sys/class/power_supply/ | grep -i BAT | head -n 1)"
 AC="$(ls /sys/class/power_supply/ | grep -i AC | head -n 1)"
 
-BATTERY="$(cat /sys/class/power_supply/$BAT/capacity)"
-CHARGE="$(cat /sys/class/power_supply/$AC/online)"
+function get_battery {
+    echo $(cat /sys/class/power_supply/$BAT/capacity)
+}
 
-function battery {
-	if [[ $CHARGE == 1 ]]; then
-       hsetroot -fill $DIR/images/charge_1.png ; sleep 0.8
-       hsetroot -fill $DIR/images/charge_2.png ; sleep 0.8
-       hsetroot -fill $DIR/images/charge_3.png ; sleep 0.8
-       hsetroot -fill $DIR/images/charge_4.png ; sleep 0.8
-       hsetroot -fill $DIR/images/charge_5.png ; sleep 0.8
-          
-	elif [[ $BATTERY -ge 5 ]] && [[ $BATTERY -le 20 ]]; then
-       hsetroot -fill $DIR/images/battery_1.png ; sleep 5
-	elif [[ $BATTERY -ge 20 ]] && [[ $BATTERY -le 40 ]]; then
-       hsetroot -fill $DIR/images/battery_2.png ; sleep 5
-	elif [[ $BATTERY -ge 40 ]] && [[ $BATTERY -le 60 ]]; then
-       hsetroot -fill $DIR/images/battery_3.png ; sleep 5
-	elif [[ $BATTERY -ge 60 ]] && [[ $BATTERY -le 80 ]]; then
-       hsetroot -fill $DIR/images/battery_4.png ; sleep 5
-	elif [[ $BATTERY -ge 80 ]] && [[ $BATTERY -le 100 ]]; then
-       hsetroot -fill $DIR/images/battery_5.png ; sleep 5
+function is_charging {
+    echo $(cat /sys/class/power_supply/$AC/online)
+}
+
+function set_wallpaper {
+    hsetroot -fill $DIR/images/charge_$1.png
+}
+
+function animate_wallpaper {
+    for i in {1..5}; do
+        # cycle through images
+        set_wallpaper $i; sleep 0.8
+    done
+}
+
+function main {
+
+    BATTERY=get_battery
+    CHARGING_STATUS=is_charging
+
+    if [[ $CHARGING_STATUS == "1" ]]; then
+        animate_wall
+    else
+        num=$(($BATTERY/20))
+        set_wallpaper $num
     fi
 }
 
-while true;do
-	battery && exec $DIR/battery_wall.sh
+while true; do
+	main
 done
